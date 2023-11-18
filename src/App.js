827b28b9c8a9f7e4b38ad68c8a2a540af997bf1b
raflyyunandia <raflyyunandia@gmail.com>
2023-11-18 13:39:28 +0700

    add title route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
-import {Routes, Route} from "react-router-dom";
-import About from "./component/About";
-import Blog from "./component/Blog";
-import Contact from "./component/Contact";
-import Home from "./component/Home";
-import Navbar from "./component/Navbar";
-import Services from "./component/Services";
-import Team from "./component/Team";
-import Footer from "./component/Footer";
-
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import About from './component/About';
+import Blog from './component/Blog';
+import Contact from './component/Contact';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Services from './component/Services';
+import Team from './component/Team';
+import Footer from './component/Footer';
 
 function App() {
+  const location = useLocation();
+
+  const getTitle = React.useCallback(() => {
+    switch (location.pathname) {
+      case '/':
+        return 'Home - MyMobilize';
+      case '/blog':
+        return 'Blog - MyMobilize';
+      case '/team':
+        return 'Team - MyMobilize';
+      case '/about':
+        return 'About Us - MyMobilize';
+      case '/services':
+        return 'Our Services - MyMobilize';
+      case '/contact':
+        return 'Contact Us - MyMobilize';
+      default:
+        return 'MyMobilize';
+    }
+  }, [location.pathname]);
+
+  const getDescription = React.useCallback(() => {
+    switch (location.pathname) {
+      case '/':
+        return 'Welcome to our website. Learn more about our services and products.';
+      case '/blog':
+        return 'Read our latest blog posts for insightful information and updates.';
+      case '/team':
+        return 'Meet our talented team members who drive our company forward.';
+      case '/about':
+        return 'Discover more about our company and its mission.';
+      case '/services':
+        return 'Explore the various services we offer to meet your needs.';
+      case '/contact':
+        return 'Get in touch with us for any queries or collaborations.';
+      default:
+        return 'Your Website Description';
+    }
+  }, [location.pathname]);
+
+  React.useEffect(() => {
+    document.title = getTitle();
+    // Update other meta tags based on the route if needed
+    document.querySelector('meta[name="description"]').setAttribute('content', getDescription());
+  }, [getTitle, getDescription]);
+
   return (
     <>
-    <Navbar />
-
-    <Routes >
-      <Route path="/" element={ <Home /> } />
-      <Route path="/blog" element={ <Blog /> } />
-      <Route path="/team" element={ <Team /> } />
-      <Route path="/about" element={ <About /> } />
-      <Route path="/services" element={ <Services /> } />
-      <Route path="/contact" element={ <Contact /> } />
-    </Routes>
-
-    <Footer />
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/component/Blog.js
+++ src/component/Blog.js
@@ -20,7 +20,7 @@ function Blog() {
                     <a href="/" className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                       <p className="pr-2 text-sm font-medium leading-none">Read More</p>
                       <svg className="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                       </svg>
                     </a>
                   </div>
@@ -36,7 +36,7 @@ function Blog() {
                     <a href="/" className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                       <p className="pr-2 text-sm font-medium leading-none">Read More</p>
                       <svg className="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                       </svg>
                     </a>
                   </div>
@@ -53,7 +53,7 @@ function Blog() {
                   <a href="/" className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                     <p className="pr-2 text-sm font-medium leading-none">Read More</p>
                     <svg className="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                      <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                   </a>
                 </div>
@@ -72,7 +72,7 @@ function Blog() {
                   <a href="/" className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                     <p className="pr-2 text-sm font-medium leading-none">Read More</p>
                     <svg className="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                      <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                   </a>
                 </div>
@@ -90,7 +90,7 @@ function Blog() {
                     <a href="/" className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                       <p className="pr-2 text-sm font-medium leading-none">Read More</p>
                       <svg className="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                       </svg>
                     </a>
                   </div>
@@ -106,7 +106,7 @@ function Blog() {
                     <a href="/" className="focus:outline-none focus:underline flex items-center mt-4 cursor-pointer text-white hover:text-gray-200 hover:underline">
                       <p className="pr-2 text-sm font-medium leading-none">Read More</p>
                       <svg className="fill-stroke" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M5.75 12.5L10.25 8L5.75 3.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                       </svg>
                     </a>
                   </div>
